320950a128166dfa2e7cde6f3dd7767cf8b0c105
MaxLindblom <max.g.lindblom@gmail.com>
2021-04-18 13:51:38 +0200

    Shell endpoints set up

--- main.py
+++ main.py
@@ -1,8 +1,102 @@
+"""Main class containing all exposed endpoints"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.post("/mail/{email}")
+async def register_email(email, lat, long):
+    """Registers a client based on email
 
-@app.get("/")
-async def init():
-    return {"msg": "hello world"}
+    Parameters
+    ----------
+    email : str
+        The email to register
+    lat : int
+        Lattitude for the client, closest integer
+    long: int
+        Longitude for the client, closest integer
+
+    Returns
+    -------
+    """
+
+@app.post("/phone/{phone}")
+async def register_phone(phone, lat, long):
+    """Registers a client based on phone number
+
+    Parameters
+    ----------
+    phone : str
+        The phone number to register
+    lat : int
+        Lattitude for the client, closest integer
+    long: int
+        Longitude for the client, closest integer
+
+    Returns
+    -------
+    """
+
+
+@app.put("/mail/{email}")
+async def update_geolocation_email(email, lat, long):
+    """Updates geolocation info for an email
+
+    Parameters
+    ----------
+    email : str
+        The email to update info for
+    lat : int
+        Lattitude for the client, closest integer
+    long: int
+        Longitude for the client, closest integer
+
+    Returns
+    -------
+    """
+
+
+@app.put("/phone/{phone}")
+async def update_geolocation_phone(phone, lat, long):
+    """Updates geolocation info for a phone nubmer
+
+    Parameters
+    ----------
+    phone : str
+        The phone number to update info for
+    lat : int
+        Lattitude for the client, closest integer
+    long: int
+        Longitude for the client, closest integer
+
+    Returns
+    -------
+    """
+
+
+@app.delete("/mail/{email}")
+async def delete_email(email):
+    """Unsubscribes an email from the service
+
+    Parameters
+    ----------
+    email : str
+        The email to unsubscribe
+
+    Returns
+    -------
+    """
+
+
+@app.delete("/phone/{phone}")
+async def delete_phone(phone):
+    """Unsubscribes a phone number from the service
+
+    Parameters
+    ----------
+    phone : str
+        The phone number to unsubscribe
+
+    Returns
+    -------
+    """
